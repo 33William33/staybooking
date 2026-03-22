47a784d18f66e593c4703b1c2fc18cad6ca094df
33William33 <yunfei.wang@mail.utoronto.ca>
2026-03-22 02:49:57 -0400

    build user table backend

---
+++ src/main/java/com/self/staybooking/model/UserRepository.java
@@ -0,0 +1,4 @@
+package com.self.staybooking.model;
+
+public interface UserRepository {
+}
---
+++ src/main/java/com/self/staybooking/model/UserRole.java
@@ -0,0 +1,6 @@
+package com.self.staybooking.model;
+
+public enum UserRole {
+    ROLE_HOST,
+    ROLE_GUEST,
+}
