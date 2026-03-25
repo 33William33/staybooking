9b55485f49dccd24db1ec2f6a7a8557c15f5b936
33William33 <yunfei.wang@mail.utoronto.ca>
2026-03-25 00:16:16 -0400

    create booking controller api

---
+++ src/main/java/com/self/staybooking/model/BookingRequest.java
@@ -0,0 +1,11 @@
+package com.self.staybooking.model;
+
+import java.time.LocalDate;
+
+public record BookingRequest(
+        long listingId,
+        LocalDate checkInDate,
+        LocalDate checkOutDate
+) {
+}
+
